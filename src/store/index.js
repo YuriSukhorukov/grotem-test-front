fc4e39ed66b2c8b3b89e0b3ea593c2707cf9a39e
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-01 16:11:56 +0300

    Минимизировано основное хранилище.

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cart from './modules/cart'
+import products from './modules/products'
 
 Vue.use(Vuex)
 
-const store = new Vuex.store({
+const store = new Vuex.Store({
+	modules: {
+		cart,
+		products
+	},
 	state: {
-		results: []
+
+	},
+	getters: {
+
+	},
+	mutations: {
+
 	},
 	actions: {
-		seartch({ state, dispatch, commit}, query) {
-			dispatch('seartch', 'фонарь') // action
-			commit('set', 'фонарь') // mutation (set, change, delete)
-		}
+
 	}
 })
+
+export default store
