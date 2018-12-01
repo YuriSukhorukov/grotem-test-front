ac08410f66e8bf1fd913b12abeb0cf0c2d41feba
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-01 20:15:49 +0300

    Конвертация данных о товарах при загрузке. Очистка null категорий.

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -21,44 +21,34 @@ const products = {
   actions: { 
   	loadWithParent({ commit }) {
   	  shop.getProductsWithParent().then(result => {
-  	  	commit('setProducts', result.data)
+          commit('setProducts', result.data.map(val => {
+          if(val.group !== null){
+            val.skus.forEach(v => {
+              v.group = val.group.name
+              v.selected = false
+              return v
+            })
+            return val.skus
+          }
+        }).flat().filter(val => val != null))
   	  })
     },
     loadParents({ commit }) {
       shop.getProductsParents().then(result => {
-        commit('setGroups', result.data)
+        commit('setGroups', result.data.filter(g => g.name !== null && g.name.match(/^([^0-9]*)$/)))
       })
     }
   },
 
   getters: { 
   	all: state => {
-  	  return state.all.map(val => {
-  	  	if(val.group !== null){
-  	  		val.skus.forEach(v => {
-  	  			v.group = val.group.name
-  	  			return v
-  	  		})
-  	  		return val.skus
-  	  	}
-  	  }).flat().filter(val => val != null)
+      return state.all
   	},
   	byGroup: state => name => {
-  	  return state.all.filter(products => {
-  	  	if(products.group != null) 
-  	  		return products.group.name === name
-  	  }).map(val => {
-  	  	if(val.group != null){
-  	  		val.skus.forEach(v => {
-  	  			v.group = val.group.name
-  	  			return v
-  	  		})
-  	  		return val.skus
-  	  	}
-  	  }).flat().filter(val => val != null)
+      return state.all.filter(product => product.group === name)
   	},
   	groups: state => {
-  	  return state.groups.filter(g => g.name !== null)
+      return state.groups
   	}
   }
 }
