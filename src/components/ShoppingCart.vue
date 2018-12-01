e29116213f75f8f7f49eaa7225672969698a1ba8
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-12-01 20:53:21 +0300

    Отображение в корзине списка добавленных товаров.

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,11 +1,33 @@
 <template>
-	<div id="shopping-cart"></div>
+	<div id="shopping-cart">
+		<table>
+	    <thead>
+	      <tr>
+	        <th>Категория</th>
+	        <th>Товар</th>
+	        <th>Цена</th>
+	      </tr>
+	    </thead>
+	    <tbody>
+	      <tr v-for="product in added">
+	        <td>{{product.group}}</td>
+	        <td>{{product.name}}</td>
+	        <td>{{product.price}}</td>
+	      </tr>
+	    </tbody>
+	  </table>
+	</div>
 </template>
 
 <script>
 import { mapState, mapActions, mapGetters } from 'vuex'
 export default {
-	name: 'ShoppingCart'	
+	name: 'ShoppingCart',
+	computed: {
+		...mapGetters('cart', [
+			'added'
+		])
+	}
 };
 </script>
 
